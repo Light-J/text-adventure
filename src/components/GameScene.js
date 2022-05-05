d24cf952a77f9ef0c49623ce3f4c4ec4d38624c8
Jack <jpjlight@gmail.com>
2022-05-05 21:56:15 +0100

    Basic functional game with ci cd

--- src/components/GameScene.js
+++ src/components/GameScene.js
@@ -1,17 +1,29 @@
 import React from "react";
 import Action from "./Action";
+import styled from 'styled-components';
 
+const SceneScreen = styled.div`
+`;
+
+const ActionPanel = styled.div`
+`;
 
 const GameScene = ({sceneDetails, updateGameScene}) => {
 
     return <div>
-            <p>{sceneDetails.text}</p>
-            {sceneDetails.actions.map((action, id) => 
-                <Action 
-                key={id}
-                updateGameScene={updateGameScene}
-                text={action.text}
-                location={action.location} />)}
+            <SceneScreen>
+                <p>{sceneDetails.text}</p>
+            </SceneScreen>
+            
+            <ActionPanel>
+                {sceneDetails.actions.map((action, id) => 
+                        <Action 
+                        key={id}
+                        updateGameScene={updateGameScene}
+                        text={action.text}
+                        destination={action.destination} />)}
+            </ActionPanel>
+
         </div>
 }
 
--- src/components/ResetButton.js
+++ src/components/ResetButton.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 import styled from 'styled-components';
 
 const Button = styled.button`
   align-items: center;
   appearance: none;
-  background-color: #FCFCFD;
+  background-color: #61000a;
   border-radius: 4px;
   border-width: 0;
-  box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#D6D6E7 0 -3px 0 inset;
+  box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#3b0006 0 -3px 0 inset;
   box-sizing: border-box;
-  color: #36395A;
+  color: #ffffff;
   cursor: pointer;
   display: inline-flex;
   font-family: "JetBrains Mono",monospace;
@@ -30,29 +30,25 @@ const Button = styled.button`
   white-space: nowrap;
   will-change: box-shadow,transform;
   font-size: 18px;
-  margin: 10px;
 
 
     &:focus {
-    box-shadow: #D6D6E7 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+    box-shadow: #3b0006 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #3b0006 0 -3px 0 inset;
     }
 
     &:hover {
-    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #3b0006 0 -3px 0 inset;
     transform: translateY(-2px);
     }
 
     &:active {
-    box-shadow: #D6D6E7 0 3px 7px inset;
+    box-shadow: #3b0006 0 3px 7px inset;
     transform: translateY(2px);
     }
 `;
 
-const Action = ({text, destination, updateGameScene}) => {
-
-    return <Button onClick={() => updateGameScene(destination)}>{text}</Button>
-
-
+const ResetButton = ({onReset}) => {
+    return <Button onClick={() => onReset()}>Restart game</Button>
 }
 
-export default Action
+export default ResetButton;
