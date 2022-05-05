2f9039809dac6a434f3bf87e213361377c6e3dca
Jack <jpjlight@gmail.com>
2022-05-05 22:00:25 +0100

    Remove unused dep

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import GameScene from "../components/GameScene";
 import ResetButton from "../components/ResetButton";
 import gameData from "../data/test-game.json"
